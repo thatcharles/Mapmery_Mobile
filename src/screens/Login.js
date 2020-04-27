f8f9fcd509a5aed73ec06934e186628e11584626
Charles Chung <charles5j21z@gmail.com>
2020-04-27 14:34:09 -0400

    animated login frontend

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,10 +1,10 @@
 import React, { useState, useMemo, useEffect } from 'react'
-import { StyleSheet, View, FlatList, Text, Image, Dimensions, TouchableWithoutFeedback } from 'react-native'
-import { List } from 'react-native-paper';
+import { StyleSheet, View, Text, Dimensions, TouchableWithoutFeedback, TextInput } from 'react-native'
 import { Asset } from 'expo-asset';
 import { AppLoading } from 'expo';
 import Animated, { Easing } from 'react-native-reanimated';
 import { TapGestureHandler, State } from 'react-native-gesture-handler';
+import Svg, {Image, Circle, ClipPath} from 'react-native-svg';
 
 const {
   useCode,
@@ -23,7 +23,8 @@ const {
   interpolate,
   Extrapolate,
   and,
-  not
+  not,
+  concat
 } = Animated;
 
 const cacheImages = (images) => {
@@ -71,9 +72,8 @@ const {width,height} = Dimensions.get('window')
 export default function Login() {
 
   const [isReady, setIsReady] = useState(false)
-  const [show, setShow] = useState()
 
-  // load image inot cache before loading the page
+  // load image into cache before loading the page
   const _loadAssetsAsync = async() => {
     const imageAssets = cacheImages([require('../../assets/img/rotterdam.jpg')]);
 
@@ -82,7 +82,7 @@ export default function Login() {
   }
 
   /**
-   *  For animation 
+   *  Method 1. Utilize useEffect and useCode to change buttonOpacity with animation. Need to maintain multiple variables. Less clean.
    * */ 
   /*
   const {clock, isShow, buttonOpacity} = useMemo(
@@ -93,11 +93,9 @@ export default function Login() {
     }),
     [],
   );
-  */
-   
+
   const [clock, setClock] = useState(new Clock())
   const [isShow, setIsShow] = useState(new Value(1))
-  const [buttonOpacity, setButtonOpacity] = useState(new Value(1))
 
   useEffect(() => {
     isShow.setValue(show ? 1 : 0);
@@ -111,7 +109,40 @@ export default function Login() {
     ]),
     [isShow]
   );
+  */
+   
+  const [buttonOpacity, setButtonOpacity] = useState(new Value(1))
 
+  /**
+   * Method 2. Use Gesture Handler to change buttonOpacity with animation. Less variables. Clean and easy to read.
+   */
+  const onStateChange = event([
+    {
+      nativeEvent: ({ state }) =>
+        block([
+          cond(
+            eq(state, State.END),
+            set(buttonOpacity, runTiming(new Clock(), 1, 0))
+          )
+        ])
+    }
+  ]);
+
+  const onCloseState = event([
+    {
+      nativeEvent: ({ state }) =>
+        block([
+          cond(
+            eq(state, State.END),
+            set(buttonOpacity, runTiming(new Clock(), 0, 1))
+          )
+        ])
+    }
+  ]);
+  
+  /**
+   * inputRange would take buttonOpacity in and outputRange is the corresponding output value
+   */
   const buttonY = interpolate(buttonOpacity, {
     inputRange: [0, 1],
     outputRange: [100, 0],
@@ -120,7 +151,31 @@ export default function Login() {
 
   const bgY = interpolate(buttonOpacity, {
     inputRange: [0, 1],
-    outputRange: [-height / 3, 0],
+    outputRange: [-height / 3 - 50, 0],
+    extrapolate: Extrapolate.CLAMP
+  });
+
+  const textInputZIndex = interpolate(buttonOpacity, {
+    inputRange: [0, 1],
+    outputRange: [1, -1],
+    extrapolate: Extrapolate.CLAMP
+  });
+
+  const textInputOpacity = interpolate(buttonOpacity, {
+    inputRange: [0, 1],
+    outputRange: [1, 0],
+    extrapolate: Extrapolate.CLAMP
+  });
+
+  const textInputY = interpolate(buttonOpacity, {
+    inputRange: [0, 1],
+    outputRange: [0, 100],
+    extrapolate: Extrapolate.CLAMP
+  });
+
+  const rotateCross = interpolate(buttonOpacity, {
+    inputRange: [0, 1],
+    outputRange: [180, 360],
     extrapolate: Extrapolate.CLAMP
   });
 
@@ -134,27 +189,77 @@ export default function Login() {
             transform: [{ translateY: bgY }]
           }}
         >
-          <Image
-            source={require('../../assets/img/rotterdam.jpg')}
-            style={styles.titleImage}
-          />
+          <Svg height={height + 50} width={width}>
+            <ClipPath id='clip'>
+              <Circle r={height + 50} cx={width / 2}/>
+            </ClipPath>
+            <Image
+              href={require('../../assets/img/rotterdam.jpg')}
+              width={width}
+              height={height + 50}
+              preserveAspectRatio= 'xMidYMid slice' // preserveAspectRatio set image to fill the screen
+              clipPath='url(#clip)'
+            />
+          </Svg>
         </Animated.View>
         <View style={styles.bottomArea}>
+          {/** Legacy for method 1.
+           * 
           <TouchableWithoutFeedback onPress={() => {
             setShow(!show)
-            // console.log(show)
           }}>
             <Animated.View style={{...styles.button, 
                                   opacity: buttonOpacity,
-                                  transform: [{ translateY: buttonY }]}}>
+                                  transform: [{ translateY: buttonY }]}}
+            >
               <Text style={{ fontSize: 20, fontWeight: 'bold' }}>SIGN IN</Text>
             </Animated.View>
           </TouchableWithoutFeedback>
+          */}
+          <TapGestureHandler onHandlerStateChange={onStateChange}>
+            <Animated.View style={{...styles.button, 
+                                  opacity: buttonOpacity,
+                                  transform: [{ translateY: buttonY }]}}
+            >
+              <Text style={{ fontSize: 20, fontWeight: 'bold' }}>SIGN IN</Text>
+            </Animated.View>
+          </TapGestureHandler>
           <Animated.View style={{...styles.button, 
                                 backgroundColor: '#2E71DC',
                                 opacity: buttonOpacity,
-                                transform: [{ translateY: buttonY }]}}>
+                                transform: [{ translateY: buttonY }]}}
+          >
             <Text style={{ fontSize: 20, fontWeight: 'bold', color:'white' }}>SIGN IN WITH FACEBOOK</Text>
+          </Animated.View>
+          <Animated.View style={{zIndex: textInputZIndex,
+                                opacity: textInputOpacity,
+                                transform: [{translateY: textInputY}],
+                                height: height / 3,
+                                ...StyleSheet.absoluteFill,
+                                top: null,
+                                justifyContent: 'center'}}
+          >
+            <TapGestureHandler onHandlerStateChange={onCloseState}>
+              <Animated.View style={styles.closeButton}>
+                <Animated.Text style={{fontSize: 15,
+                                        transform: [{rotate: concat(rotateCross,'deg')}]}}>
+                  X
+                </Animated.Text>
+              </Animated.View>
+            </TapGestureHandler>
+            <TextInput 
+              placeholder="EMAIL"
+              style={styles.textInput}
+              placeholderTextColor="black"
+            />
+            <TextInput 
+              placeholder="PASSWORD"
+              style={styles.textInput}
+              placeholderTextColor="black"
+            />
+            <Animated.View style={styles.button}>
+              <Text style={{fontSize:20, fontWeight:'bold'}}>SIGN IN</Text>
+            </Animated.View>
           </Animated.View>
         </View>
       </View>
@@ -199,6 +304,33 @@ const styles = StyleSheet.create({
     borderRadius: 35,
     alignItems: 'center',
     justifyContent: 'center',
-    marginVertical: 5
+    marginVertical: 5,
+    shadowOffset: {width: 2, height: 2},
+    shadowColor: 'black',
+    shadowOpacity: 0.2
+  },
+  closeButton: {
+    height: 40,
+    width: 40,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: -20,
+    left: width / 2 - 20,
+    shadowOffset: {width: 2, height: 2},
+    shadowColor: 'black',
+    shadowOpacity: 0.2
+  },
+  textInput: {
+    height: 50,
+    marginHorizontal: 20,
+    backgroundColor: 'white',
+    borderRadius: 25,
+    borderWidth: 0.5,
+    paddingLeft: 10,
+    marginVertical: 5,
+    borderColor: 'rgba(0,0,0,0.2)'
   }
 })
